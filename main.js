e57a176a8a4d635b5a41dd70408e28658e4d5170
Serge Ilyashenko <serj.ilyashenko@gmail.com>
2021-03-13 20:21:41 +0300

    feat(SVGPie): set sector sizes;

--- main.js
+++ main.js
@@ -3,6 +3,14 @@
   const PI = Math.PI;
   const radius = 50;
 
+  const defaultPallet = [
+    "#69D2E7","#A7DBD8","#E0E4CC","#F38630","#FA6900", "#FE4365","#FC9D9A","#F9CDAD","#C8C8A9","#83AF9B",
+    "#ECD078","#D95B43","#C02942","#542437","#53777A", "#556270","#4ECDC4","#C7F464","#FF6B6B","#C44D58",
+    "#774F38","#E08E79","#F1D4AF","#ECE5CE","#C5E0DC", "#E8DDCB","#CDB380","#036564","#033649","#031634",
+    "#490A3D","#BD1550","#E97F02","#F8CA00","#8A9B0F", "#FF9900","#424242","#E9E9E9","#BCBCBC","#3299BB",
+    "#5D4157","#838689","#A8CABA","#CAD7B2","#EBE3AA", "#5E412F","#FCEBB6","#78C0A8","#F07818","#F0A830",
+  ];
+
   function svgSectorFactory(radAngle0, radAngleDiff, color) {
     const x0 = radius + radius * Math.sin(radAngle0);
     const y0 = radius - radius * Math.cos(radAngle0);
@@ -27,7 +35,7 @@
 
   function svgWrapperFactory(element, borderColor = 'black') {
     return `
-        <svg viewBox="-10 -10 120 120" xmlns="http://www.w3.org/2000/svg">
+        <svg viewBox="-5 -5 110 110" xmlns="http://www.w3.org/2000/svg">
             <g stroke="${borderColor}" stroke-width="2px" fill="transparent">
                 ${element}
             </g>
@@ -36,17 +44,26 @@
   }
 
   class SVGPie {
-    constructor($id, colors, borderColor = 'black') {
+    constructor($id, values, pallet = defaultPallet, borderColor = 'black') {
       const pieElement = document.createElement('div');
 
       pieElement.style.width = '100%';
       pieElement.style.height = '100%';
 
-      const radAngle = 2 * PI / colors.length;
-      const sectorElementList = colors.map((color, index) => {
-        const radAngle0 = radAngle * index;
+      const _values = values || new Array(pallet.length).fill(1);
+      const sum = _values.reduce((s, x) => Number(s) + Number(x), 0);
+      const angleDiffList = _values.map(v => 2 * PI * v / sum);
 
-        return svgSectorFactory(radAngle0, radAngle, color);
+      const angleCoordinates = [];
+      let anglePosition = 0;
+      for (let i in angleDiffList) {
+        const angleDiff = angleDiffList[i];
+        angleCoordinates.push([anglePosition, angleDiff]);
+        anglePosition += Number(angleDiff);
+      }
+
+      const sectorElementList = angleCoordinates.map(([angle0, angleDiff], index) => {
+        return svgSectorFactory(angle0, angleDiff, pallet[index]);
       });
 
       pieElement.innerHTML = svgWrapperFactory(sectorElementList.join(''), borderColor);
